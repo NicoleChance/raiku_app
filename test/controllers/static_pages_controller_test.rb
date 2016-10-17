b4aa48d4b9da5a7ab23a96a24a4e98d28576b1f7
NicoleChance <nicolechance94@gmail.com>
2016-10-16 21:01:11 -0400

    fixed finished static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,14 +10,14 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help"
+    assert_select "title", "Help | Raiku"
 
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About"
+    assert_select "title", "About | Raiku"
 
   end
 
